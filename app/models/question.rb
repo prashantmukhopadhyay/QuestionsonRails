d2889534fa570e31f3f1334b0a98dea76af8092c
coleman-prashant <coleman+prashant@appacademy.io>
2013-11-07 14:43:29 -0500

    add question response method

--- app/models/question.rb
+++ app/models/question.rb
@@ -15,7 +15,23 @@ class Question < ActiveRecord::Base
     primary_key: :id
   )
 
+  def results
+    results_with_counts = self
+         .answer_choices
+         .select("answer_choices.*, COUNT(*) AS response_count")
+         .group("answer_choices.id")
 
+    results_hash = {}
+    results_with_counts.each do |result|
+      results_hash[result.answer_text] = result.response_count.to_i
+    end
+
+    results_hash
+  end
+
+# User.select("users.*").
+# joins(authored_polls: [questions: :answer_choices]).
+# where("answer_choices.id = ?", 2)
 
 
 end
--- app/models/response.rb
+++ app/models/response.rb
@@ -2,6 +2,7 @@ class Response < ActiveRecord::Base
 
   validates :user_id, :answer_choice_id, presence: true
   validate :respondent_has_not_already_answered_question
+  validate :respondent_is_not_the_poll_author
 
   def respondent_has_not_already_answered_question
     query = "
@@ -14,13 +15,13 @@ class Response < ActiveRecord::Base
       WHERE r.user_id = ?
       AND q.id = ?
     "
-    reponse = Response.find_by_sql([query, self.user_id, q_id])
+    response = Response.find_by_sql([query, self.user_id, q_id])
 
     errors[:base] << "Cannot answer same question twice." if !response.empty? && self.id.nil?
   end
 
   def respondent_is_not_the_poll_author
-
+    errors[:base] << "Cannot answer your own poll." if self.user_id == poll_author_id
   end
 
   belongs_to(
@@ -44,7 +45,10 @@ class Response < ActiveRecord::Base
     end
 
     def poll_author_id
+      poll_authors = #should only be one
+      User.select("users.*").joins(authored_polls: [questions: :answer_choices]).where("answer_choices.id = ?", 2)
 
+      poll_authors[0].id # works!
     end
 
 end
